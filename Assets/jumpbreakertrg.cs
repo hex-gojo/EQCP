f57a1923fdedecba7842b7b98a0f0063504d8318
田中龍斗 <ryoto@MacBook-Pro-2.local>
2019-01-11 14:17:53 +0900

    シーン切り替えの追加

---
+++ Assets/jumpbreakertrg.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jumpbreakertrg : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("aaaa");
+		SceneManager.LoadScene ("Event1");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
