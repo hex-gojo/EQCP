a4ea9618566129e225c1a1764a0e838a22960d25
gojo <m1m18a11@st.oit.ac.jp>
2019-01-11 18:24:05 +0900

    Resultシーン: 新規作成 & スクリプトファイル移動

---
+++ Assets/Scripts/JumpStart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class JumpStart : MonoBehaviour {
+
+	public void JumpScene() {
+		SceneManager.LoadScene ("Start");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public GameObject score_object = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Text Score_text = score_object.GetComponent<Text> ();
+		Score_text.text = "評価点 : " + Dataset.score + " / 5 点";
+	}
+}
