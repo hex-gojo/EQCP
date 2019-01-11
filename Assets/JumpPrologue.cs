491384cf7d4d1d108184e3c98549b04f7780b2db
ryoto <ryoto@MacBook-Pro-2.local>
2019-01-11 19:19:53 +0900

    EQCP完成

---
+++ Assets/JumpPrologue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class JumpPrologue : MonoBehaviour {
+	public void GameStart() {
+		SceneManager.LoadScene ("Prologue");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/JumpSceneScript/resulttrg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resulttrg : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+			SceneManager.LoadScene ("Result");
+			Vector3 tmp = GameObject.Find ("PQchan").transform.position;
+			Dataset.playerX = tmp.x + 1;
+		Dataset.playerZ = tmp.z;
+			tmp = GameObject.Find ("PQchan").transform.localEulerAngles;
+			Dataset.Rotation = tmp.y;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
