5a2f52a4c960ca207c38f51eae6e77cb500f64c3
gojo <m1m18a11@st.oit.ac.jp>
2019-01-11 14:11:27 +0900

    Event1: シーン移動機能追加

---
+++ Assets/JumpScene2.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class JumpScene2 : MonoBehaviour {
+
+	public void JumpScene() {
+		SceneManager.LoadScene ("Main");
+	}
+}
